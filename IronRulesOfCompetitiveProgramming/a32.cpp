4d0b6e380d77529506db82a369d90922704aec87
ykagano <y.kagano@gmail.com>
2025-06-22 23:29:33 +0900

    chore: a32を追加

---
+++ IronRulesOfCompetitiveProgramming/a32.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 配列 dp について: dp[x]=true の時勝ちの状態。dp[x]=false の時負けの状態
+int N, A, B;
+bool dp[100009];
+
+int main() {
+  // 入力
+  cin >> N >> A >> B;
+
+  // 勝者を計算する
+  for (int i = 0; i <= N; i++) {
+    if (i >= A && dp[i-A] == false) dp[i] = true; // 勝ちの状態
+    else if (i >= B && dp[i-B] == false) dp[i] = true; // 勝ちの状態
+    else dp[i] = false; // 負けの状態
+  }
+
+  // 出力
+  if (dp[N] == true) cout << "First" << endl;
+  else cout << "Second" << endl;
+  return 0;
+}
