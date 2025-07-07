81d2f5f1395dc11716d7d9f691774588c8743a88
ykagano <y.kagano@gmail.com>
2025-07-07 11:53:34 +0900

    chore: a43を追加

---
+++ IronRulesOfCompetitiveProgramming/a43.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, L;
+int A[200009];
+char B[200009];
+
+int main() {
+  // 入力
+  cin >> N >> L;
+  for (int i = 1; i <= N; i++) cin >> A[i] >> B[i];
+
+  // 答えを求める
+  int Answer = 0;
+  for (int i = 1; i <= N; i++) {
+    if (B[i] == 'E') Answer = max(Answer, L - A[i]);
+    if (B[i] == 'W') Answer = max(Answer, A[i]);
+  }
+
+  // 出力
+  cout << Answer << endl;
+  return 0;
+}
